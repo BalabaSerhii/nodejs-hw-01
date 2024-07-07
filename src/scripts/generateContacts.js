256c18b4f8b753e0d44ef68cf297653ab2bb1a2c
Serhii Balaba <balabaserhii@gmail.com>
2024-07-07 21:36:56 +0200

    s

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs.promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fr.readFile(PATH_DB, 'utf8');
+    const constants = JSON.parse(data);
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+    const patchContacts = [...constants, ...newContacts];
+    await fs.writeFile(PATH_DB, JSON.stringify(patchContacts, null, 4), 'utf8');
+  } catch (error) {
+    console.error('Error generating contacts', error);
+  }
+};
 
 generateContacts(5);
