931166cae18067168f25dab2a697bdf1862de6e6
Serhii Balaba <balabaserhii@gmail.com>
2024-07-07 22:30:29 +0200

    super

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    const rez = `Number of contacts: ${contacts.length}`;
+    console.log('first', contacts);
+    return rez;
+  } catch (error) {
+    console.error('Error ADD contacts', error);
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    if (contacts.lenght > 0) {
+      const removeLast = contacts.pop();
+      console.log(removeLast);
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 4), 'utf8');
+    } else {
+      console.log('There are no contacts to delete.');
+    }
+  } catch (error) {
+    console.error('Error remove last contacts', error);
+  }
+};
 
 removeLastContact();
